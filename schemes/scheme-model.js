8257e2999bf8ebc159674e3572c4570c7f7dd967
Tyler Biswell <tsbiswell@gmail.com>
2019-10-10 09:20:13 -0600

    added find() helper method

---
+++ schemes/scheme-model.js
@@ -0,0 +1,27 @@
+const db = require('../data/db-config');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+  addStep,
+};
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {}
+
+function findSteps(id) {}
+
+function add(schema) {}
+
+function update(changes, id) {}
+
+function remove(id) {}
+
+function addStep(step, scheme_id) {}
