7e28855139d77208bebbb283220b8241d131b7dc
Tyler Biswell <tsbiswell@gmail.com>
2019-10-10 09:22:01 -0600

    findSteps(id) added

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,22 +10,35 @@ module.exports = {
   addStep,
 };
 
+// FUNCTIONAL
 function find() {
   return db('schemes');
 }
 
+// FUNCTIONAL
 function findById(id) {
-    return db('schemes')
-      .where({ id })
-      .first();
-  }
+  return db('schemes')
+    .where({ id })
+    .first();
+}
 
-function findSteps(id) {}
+// FUNCTIONAL
+function findSteps(id) {
+  return db('steps')
+    .join('schemes', 'schemes.id', 'steps.scheme_id')
+    .select('steps.id', 'scheme_name', 'step_number', 'instructions')
+    .where({ scheme_id: id })
+    .orderBy('step_number');
+}
 
+//
 function add(schema) {}
 
+//
 function update(changes, id) {}
 
+//
 function remove(id) {}
 
+//
 function addStep(step, scheme_id) {}
